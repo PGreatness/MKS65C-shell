c1bcca64d5945ecca5f3a6d677fb75179a336451
Ahnaf Hasan <ahasan1@stuy.edu>
2018-12-01 13:38:02 -0500

    add working version of shell

--- forkin.c
+++ forkin.c
@@ -12,20 +12,23 @@ char ** parse_args( char * line ) {
 
 
 int deal_with_command(char * command) {
+  char ** args = parse_args(command);
   int parent = fork();
-  printf("we are here\n");
+  //printf("we are here\n");
   if(parent){
-      printf("Got here, parent\n");
+      //printf("Got here, parent\n");
       int status = 0;
       wait(&status);
+      //if (!(strcmp(args[0], "cd"))) {
+	//char ** first = strsep(args, "\0");
+	//chdir(strsep(args[
       return WEXITSTATUS(status);
     }
   else {
-     printf("Got here, child\n");
-     char ** args = parse_args(command);
+     //printf("Got here, child\n");
      //printf("%s\n", );
      execvp(args[0], args);
-     printf("this shouldnt print\n");
+     printf("%s : Unknown command. Type \"help\" for more commands\n", args[0]);
      return 0;
    }
 }
--- shell.c
+++ shell.c
@@ -7,13 +7,15 @@ int main() {
      char str[256];
      printf("YeOldUsr: ");
      fgets(str, 256, stdin);
-     char * index = strchr(str, '\n');
-     index = NULL;
+     //printf("Got here\n");
+     char * tmp_pointer = str;
+     char * prev = strsep(&tmp_pointer, "\n");
+     //printf("This is prev: %s\n", prev);
      deal_with_command(str);
-     sleep(3);
-     for (int i = 0; i < 100; i++) {
-       printf("This is str[%i]: %c\n", i, str[i]);
-    }
+     //sleep(1);
+     //for (int i = 0; i < 100; i++) {
+     //  printf("This is str[%i]: %c\n", i, str[i]);
+   // }
   }
   /* code */
   return 0;
